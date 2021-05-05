244de6432eadd74f2511e64cd74dfe6c16c83dd6
Fede <fede.bergagna@gmail.com>
2021-05-05 18:34:53 -0300

    Implementacion de Campos de Galois

---
+++ galois/galois.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdint.h>
+#include "galois.h"
+
+
+// https://en.wikipedia.org/wiki/Finite_field_arithmetic
+
+// Haciendo el truco de desplazar podes usar todo con uint8 y te ahorras espacio
+
+
+uint8_t gen_desplazado = 0b01100011;
+
+
+uint8_t main(){
+    printf("%d\n",mult_galois(84,13) );
+}
+
+uint8_t suma_galois(uint8_t x, uint8_t y){
+    return x ^ y;
+}
+
+uint8_t mult_galois(uint8_t x, uint8_t y){
+
+    uint8_t p = 0;
+    for (; y ; y >>= 1) {
+        if(y & 0b00000001){
+            p ^= x;
+        }
+        if (x & 0b10000000){
+            x <<= 1;
+            x ^= gen_desplazado;
+        }
+        else{
+            x <<= 1;
+        }
+    }
+
+
+    return p;
+
+}
---
+++ galois/galois.h
@@ -0,0 +1,3 @@
+uint8_t mult_galois(uint8_t x, uint8_t y);
+
+uint8_t suma_galois(uint8_t x, uint8_t y);
