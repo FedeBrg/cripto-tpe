e176271c73562c413b8d5aa4d73904f2a39b79c1
Juan <juanmartinbaader@hotmail.com>
2021-05-26 17:13:32 -0300

    drecript lagrange 2

--- decrypt/decrypt.c
+++ decrypt/decrypt.c
@@ -22,16 +22,14 @@ void decrypt(int k, char path[]){
 //    images = read_bmps("../manejo_bmp/test_bmps/",k);
     //leo imagenes
 
+//    images=read_bmps("../manejo_bmp/secret",k);
 
-
-    ImageBMP * portadora1 = read_bmp_new("../manejo_bmp/secret/Alfredshare.bmp");
-    printf("\n%d,%d\n",portadora1->header.biHeight,portadora1->header.biWidth);
-    printf("\n%d,%d\n",portadora1->header.biHeight,portadora1->header.biWidth);
-    ImageBMP * portadora2 = read_bmp_new("../manejo_bmp/secret/Evashare.bmp");
-    ImageBMP * portadora3 = read_bmp_new("../manejo_bmp/secret/Facundoshare.bmp");
-    ImageBMP * portadora4 = read_bmp_new("../manejo_bmp/secret/Gustavoshare.bmp");
-    ImageBMP * portadora5 = read_bmp_new("../manejo_bmp/secret/Marilynshare.bmp");
-    ImageBMP * portadora6 = read_bmp_new("../manejo_bmp/secret/Jamesshare.bmp");
+    ImageBMP * portadora1 = read_bmp("manejo_bmp/secret/Alfredshare.bmp");
+    ImageBMP * portadora2 = read_bmp("manejo_bmp/secret/Evashare.bmp");
+    ImageBMP * portadora3 = read_bmp("manejo_bmp/secret/Audreyshare.bmp");
+    ImageBMP * portadora4 = read_bmp("manejo_bmp/secret/Gustavoshare.bmp");
+    ImageBMP * portadora5 = read_bmp("manejo_bmp/secret/Marilynshare.bmp");
+    ImageBMP * portadora6 = read_bmp("manejo_bmp/secret/Jamesshare.bmp");
 
     // hago el array de punteros con la imagenes, aca despues hay que hacer el read_bmps
     ImageBMP * carriers[6];
@@ -42,45 +40,98 @@ void decrypt(int k, char path[]){
     carriers[4] = portadora5;
     carriers[5] = portadora6;
 
-    printf("1");
+//    printf("1");
 
 
     images=carriers;
 
     uint8_t *** blocks= malloc(sizeof( uint8_t*) *k); //malloc
     for (int i = 0; i < k; ++i) {
-        printf("2");
+        printf("Image: %d, size %d,%d\n",i+1,images[i]->header.biHeight,images[i]->header.biWidth);
 
         blocks[i]=split_portadora(images[i]);
     }
     //obtengo los bloques
 
-    unsigned int length=(images[0]->header.biHeight * images[0]->header.biHeight)/k;
+    unsigned int length=(images[0]->header.biHeight * images[0]->header.biHeight)/4;
 //    uint8_t y[length][k];//array con los valores de Y arrancando desde arriba a la dercha
 //    uint8_t x[length][k];//malloc?
 //    uint8_t s[length]; //probablemente sea con memeoria
     uint8_t **y= malloc(sizeof (uint8_t*)*length);
     uint8_t **x= malloc(sizeof (uint8_t*)*length);
-    uint8_t *s= malloc(sizeof (uint8_t*)*length);
-    for (int i = 0; i < (images[0]->header.biHeight * images[0]->header.biHeight)/k; ++i) {
-        x[i]= malloc(sizeof (blocks[0][0][0])*k );
-        y[i]= malloc(sizeof (blocks[0][0][0])*k );
+    uint8_t *s= images[0]->pixels;
+
+    printf("length is: %d\n",length);
+    printf("size is: %d",sizeof (uint8_t*)*length);
+
+    for (int i = 0; i < length; ++i) {
+        x[i]= malloc(sizeof (uint8_t)*k );
+        y[i]= malloc(sizeof (uint8_t)*k );
+//        printf("\n%d\n",i);
         for (int j = 0; j < k; ++j) {
-            y[i][j]=getYFromBlock( blocks[i][j][1], blocks[i][j][2], blocks[i][j][3]);
-            x[i][j]=blocks[i][j][0];
+//            printf("%d",j);
+            y[i][j]=getYFromBlock( blocks[j][i][1], blocks[j][i][2], blocks[j][i][3]);
+            x[i][j]=blocks[j][i][0];
+//            printf("x=%d, y=%d\n",x[i][j],y[i][j]);
+        }
+        if(i%1000==0){
+            printf("%d\n",i);
         }
     }
-    for (int i = 0; i < (images[0]->header.biHeight * images[0]->header.biHeight)/k; ++i) {
-        s[i]=lagrange(k,x[i],y[i]);
-        for (int j = 0; j < k; ++j) {
-            y[i][j]=(y[i][j]*s[0])/x[i][j]; //chequear que sean esos x e y ?
-            y[i][j]=div_galois(mult_galois(y[i][j],s[0]),x[i][j]); //chequear que sean esos x e y ?
+    int div0errors=0;
+    for (int i = 0; i < length; i+=8) {
+        s[i]= lagrange_turbina(k,0,x[i],y[i]);
+        if(i<length-8) {
+            for (int j = 1; j < 8; ++j) {
+                for (int l = 0; l < k; ++l) {
+                    if (x[i + j][l] != 0) {
+                        y[i + j][l] = div_galois(suma_galois(y[i + j][l], -s[i]),x[i + j][l]); //chequear que sean esos x e y ?
+                    } else {
+                        y[i + j][l] = div_galois(suma_galois(y[i + j][l], -s[i]), 255);;
+                        div0errors++;
+                    }
+                }
+                s[i + j] = lagrange_turbina(k, j, x[i + j], y[i + j]);
+            }
+            if (i % 800 == 0) {
+                printf("%d, s=%d\n", i, s[i]);
+            }
         }
     }
-    printf("asdasd");
-    ImageBMP * tmp = read_bmp("../manejo_bmp/secret/James.bmp");
-    tmp->pixels=s;
-    write_bmp(tmp,"./secret.bmp");
+
+
+//    for (int i = 0; i < length; ++i) {
+////        printf("antes de lagrange\n");
+//        s[i]=lagrange_turbina(k,i%8,x[i],y[i]);
+//
+////        printf("despues de lagrange\n");
+//        if(i%8==0 && i<length-8) {
+//            for (int l = 1; l < 8; ++l) {
+//                for (int j = 0; j < k; ++j) {
+////            printf("iteration:%d,div:(%d*%d)/%d\n",i,y[i][j],s[0],x[i][j]);
+//                    if (x[i+l][j] != 0) {
+//                        y[i+l][j] = div_galois(suma_galois(y[i+l][j], -s[0]),x[i+l][j]); //chequear que sean esos x e y ?
+//                    } else {
+//                        y[i+l][j]=div_galois(suma_galois(y[i+l][j], -s[0]),255);;
+//                        div0errors++;
+//                    }
+//                }
+//            }
+//        }
+//        if(i%1000==0){
+//            printf("%d, s=%d\n",i,s[i]);
+//        }
+//
+//    }
+    printf("\n");
+
+
+    printf("finished\n");
+
+//    ImageBMP * tmp = read_bmp("../manejo_bmp/secret/Jamesshare.bmp");
+    printf("error: divs por 0 = %d\n",div0errors);
+    images[0]->pixels=s;
+    write_bmp(images[0],"./secret.bmp");
 //    return tmp;
 }
 
@@ -109,15 +160,26 @@ unsigned char getYFromBlock( uint8_t w, uint8_t v, uint8_t u){
     //chequear pariedad
     if((w&1)^(w&2)>>1^(w&4)>>2^(v&1)^(v&2)>>1^(v&4)>>2^(u&1)^(u&2)>>1==(u&4)>>2){
         unsigned char t=0; //ejemplo w=11111111 v=10101010 y u=00000000 => w&7 = 00000111, v&7=00000010 y u&3 = 00000000 => x=11101000
-
         t=t|((w&7)<<5); //and con 00000111 para obtener los ultimos 3 bits y los corro hacia la derecha
         t=t|((v&7)<<2); //and con 00000111 para obtener los ultimos 3 bits y corro hacia la dreche 2 veces
         t=t|((u&3)); //ultimos dos bits
+//        printf("pure: %x,%x,%x,%x\n",w,v,u,t);
+//        printf("shifted: %x,%x,%x,%x\n",(w&7)<<5,(v&7)<<2,(u&3),t);
 
-        printf("parity match\n");
-        printf("t is: %x\n",t);
+//        printf("parity match! t is: %x\n",t);
         return t;
     } else{
-        printf("no parity match");
+        unsigned char t=0; //ejemplo w=11111111 v=10101010 y u=00000000 => w&7 = 00000111, v&7=00000010 y u&3 = 00000000 => x=11101000
+        t=t|((w&7)<<5); //and con 00000111 para obtener los ultimos 3 bits y los corro hacia la derecha
+        t=t|((v&7)<<2); //and con 00000111 para obtener los ultimos 3 bits y corro hacia la dreche 2 veces
+        t=t|((u&3));
+//        printf("error!\n");
+//        printf("pure: %x,%x,%x,%x\n",w,v,u,t);
+//        printf("shifted: %x,%x,%x,%x\n",(w&7)<<5,(v&7)<<2,(u&3),t);
+//        t=t|((w&7)<<5); //and con 00000111 para obtener los ultimos 3 bits y los corro hacia la derecha
+//        t=t|((v&7)<<2); //and con 00000111 para obtener los ultimos 3 bits y corro hacia la dreche 2 veces
+//        t=t|((u&3)); //ultimos dos bits
+//        printf("no parity match!t is: %x\n",t);
+        return t;
     }
 }
